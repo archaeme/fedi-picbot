0dbb70d258e3837899422562d49500104558c994
Francis Dinh <normandy@biribiri.dev>
2022-03-23 00:03:43 -0400

    initial code

---
+++ go.mod
@@ -0,0 +1,10 @@
+module codeberg.org/normandy/fedi-picbot
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/mattn/go-mastodon v0.0.4 // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mattn/go-mastodon"
+	"gopkg.in/ini.v1"
+)
+
+func register() {
+	registerCmd := flag.NewFlagSet("register", flag.ExitOnError)
+	server := registerCmd.String("server", "", "URL of server to register on")
+
+	registerCmd.Parse(os.Args[2:])
+	if *server == "" {
+		log.Fatalln("Server must be specified")
+	}
+
+	app, err := mastodon.RegisterApp(context.Background(), &mastodon.AppConfig{
+		Server:     *server,
+		ClientName: "fedi-picbot",
+		// the go-mastodon library hardcodes these scopes when authenticating, so we have to use the same ones
+		Scopes:  "read write follow",
+		Website: "https://codeberg.org/normandy/fedi-picbot",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Copy these into config.ini:")
+	fmt.Printf("Server = %s\n", *server)
+	fmt.Printf("ClientID = %s\n", app.ClientID)
+	fmt.Printf("ClientSecret = %s\n", app.ClientSecret)
+	fmt.Println("Don't forget to fill in your username and password in the config file!")
+}
+
+func post() {
+	postCmd := flag.NewFlagSet("post", flag.ExitOnError)
+	configFile := postCmd.String("config", "config.ini", "Path to config file. Defaults to config.ini in working dir")
+	sourcesFile := postCmd.String("sources", "sources.txt", "Text file that contains files to download and a link to the original source")
+	postCmd.Parse(os.Args[2:])
+
+	config, err := ini.Load(*configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	username := config.Section("Login").Key("Username").String()
+	password := config.Section("Login").Key("Password").String()
+	server := config.Section("").Key("Server").String()
+	clientID := config.Section("").Key("ClientID").String()
+	clientSecret := config.Section("").Key("ClientSecret").String()
+
+	client := mastodon.NewClient(&mastodon.Config{
+		Server:       server,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+	})
+	err = client.Authenticate(context.Background(), username, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url, source, sensitive := getUrls(*sourcesFile)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	attachment, err := client.UploadMediaFromReader(context.Background(), resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isSensitive, err := strconv.ParseBool(sensitive)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = client.PostStatus(context.Background(), &mastodon.Toot{
+		Status:    fmt.Sprintf("Source: %s", source),
+		MediaIDs:  []mastodon.ID{attachment.ID},
+		Sensitive: isSensitive,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getUrls(sourcesFile string) (url, source, sensitive string) {
+	file, err := os.Open(sourcesFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+	var pick string
+	for scanner.Scan() {
+		line := scanner.Text()
+		randomSrc := rand.NewSource(time.Now().UnixNano())
+		random := rand.New(randomSrc)
+
+		roll := random.Intn(lineNum)
+		if roll == 0 {
+			pick = line
+		}
+
+		lineNum += 1
+	}
+
+	// Line format is <image url> <source url> <sensitive bool>
+	// FIXME: maybe this could be a sqlite db or someting that isn't error prone
+	parts := strings.SplitN(pick, " ", 3)
+	url = parts[0]
+	source = parts[1]
+	sensitive = parts[2]
+	return url, source, sensitive
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("Must use 'post' or 'register' subcommands")
+	}
+
+	switch os.Args[1] {
+	case "register":
+		register()
+	case "post":
+		post()
+	default:
+		log.Fatalln("Must use 'post' or 'register' subcommands")
+	}
+}
